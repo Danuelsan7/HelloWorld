7772ebdd9328cbed2cc94becdd42ff35b64a89f9
Eduardo Moreno <eh27901@gmail.com>
2022-10-11 23:55:16 -0500

    Adding a new line on test branch

--- HelloWorldApp/Program.cs
+++ HelloWorldApp/Program.cs
@@ -7,6 +7,7 @@ namespace HelloWorldApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World I'm Here Now in test!");
+            Console.WriteLine("Adding Second Line");
         }
     }
 }
