bed48f36ea3be5bd615f904864cf137bb7d4de88
Eduardo Moreno <eh27901@gmail.com>
2022-10-24 15:34:18 -0500

    Change the output message to hello universe

--- HelloWorldApp/Program.cs
+++ HelloWorldApp/Program.cs
@@ -6,9 +6,7 @@ namespace HelloWorldApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World I'm Here Now in test!");
-            Console.WriteLine("Adding Second Line");
-            Console.WriteLine("Adding Third Line");
+            Console.WriteLine("Hello Universe!");
         }
     }
 }
