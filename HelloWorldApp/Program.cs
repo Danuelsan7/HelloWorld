e35cdf78a9832e4a4d4862112dbbcd4b47b39f8b
Eduardo Moreno <eh27901@gmail.com>
2022-10-24 23:20:58 -0500

    Trigger  my first build on teamcity

--- HelloWorldApp/Program.cs
+++ HelloWorldApp/Program.cs
@@ -7,8 +7,7 @@ namespace HelloWorldApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World I'm Here Now in test!");
-            Console.WriteLine("Adding Second Line");
-            Console.WriteLine("Adding Third Line");
+            Console.WriteLine("This change will trigger a build on teamcity");
         }
     }
 }
